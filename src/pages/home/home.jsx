faa68733584239ed76e0fe6521740131f1922c33
JamesRunnalls <james.runnalls@eawag.ch>
2021-03-26 17:30:59 +0100

    Publish updates

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,32 +6,9 @@ import * as d3 from "d3";
 import colorlist from "../../components/colors/colors";
 import { getBinaryColor } from "../../components/gradients/gradients";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module.js";
+//import Stats from "three/examples/jsm/libs/stats.module.js";
 import { apiUrl } from "../../config.json";
 import "./css/home.css";
-
-class ScaleBox extends Component {
-  render() {
-    var { label, value, onChange, s } = this.props;
-    return (
-      <table className="scale-box">
-        <tbody>
-          <tr>
-            <td>{label}</td>
-            <td>
-              <input
-                type="number"
-                value={value}
-                onChange={(e) => onChange(s, e)}
-                min={1}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-}
 
 class Home extends Component {
   state = {
@@ -48,7 +25,7 @@ class Home extends Component {
     update: false,
     mesh: true,
     lake: "geneva",
-    lakeOptions: ["geneva", "zurich"],
+    lakes: { geneva: { data: [] }, zurich: { data: [] } },
   };
 
   update = () => {
@@ -144,17 +121,18 @@ class Home extends Component {
   initialPositions = () => {
     var { arr, randomPick, depths } = this.state;
     var pl = randomPick.length - 1;
-    this.lines.forEach((line) => {
+    for (var i = 0; i < this.lines.children.length; i++) {
+      let line = this.lines.children[i];
       let pick = randomPick[Math.round(pl * Math.random())];
-      let positions = line.data.geometry.attributes.position.array;
+      let positions = line.geometry.attributes.position.array;
       positions[0] = arr[pick[0]][0]; // x
       positions[1] = depths[pick[1]]; // z
       positions[2] = -arr[pick[0]][1]; // -y
       positions[3] = positions[0];
       positions[4] = positions[1];
       positions[5] = positions[2];
-      line.data.geometry.attributes.position.needsUpdate = true;
-    });
+      line.geometry.attributes.position.needsUpdate = true;
+    }
   };
 
   indexOfClosest = (num, arr) => {
@@ -246,117 +224,6 @@ class Home extends Component {
     return colorbar;
   };
 
-  nextPosition = (xin, yin, zin) => {
-    let {
-      xSize,
-      ySize,
-      xllcorner,
-      yllcorner,
-      nCols,
-      nRows,
-      griddata,
-      velocityFactor,
-      depths,
-    } = this.state;
-    var zi = this.indexOfClosest(zin, depths);
-    var i = Math.round((yin - yllcorner) / ySize);
-    var j = Math.round((xin - xllcorner) / xSize);
-    if (i > -1 && i < nRows && j > -1 && j < nCols && griddata[i][j] !== null) {
-      var u = 0;
-      var v = 0;
-      if (griddata[i][j][0].length > 0 && griddata[i][j][0][zi] !== -999)
-        u = griddata[i][j][0][zi];
-      if (griddata[i][j][1].length > 0 && griddata[i][j][1][zi] !== -999)
-        v = griddata[i][j][1][zi];
-      var x = xin + u * velocityFactor;
-      var y = yin + -v * velocityFactor;
-      return { x, y, z: zin, u, v };
-    } else {
-      return false;
-    }
-  };
-
-  updatePositions = () => {
-    var {
-      arr,
-      randomPick,
-      depths,
-      minVelocity,
-      maxVelocity,
-      colorbar,
-      maxAge,
-      fadeOutPercentage,
-    } = this.state;
-    var fadeOut = Math.round(maxAge * fadeOutPercentage);
-    var pl = randomPick.length - 1;
-    var line_len = this.lines.length;
-    for (let i = 0; i < line_len; i++) {
-      let line = this.lines[i];
-      let positions = line.data.geometry.attributes.position.array;
-      let colors = line.data.geometry.attributes.color.array;
-      if (line.age < line.maxAge - fadeOut) {
-        // Move to next position
-        line.age++;
-        var nextposition = this.nextPosition(
-          positions[(line.age - 1) * 3],
-          positions[(line.age - 1) * 3 + 2],
-          positions[(line.age - 1) * 3 + 1]
-        );
-        if (nextposition) {
-          positions[line.age * 3] = nextposition.x;
-          positions[line.age * 3 + 1] = nextposition.z;
-          positions[line.age * 3 + 2] = nextposition.y;
-          let color =
-            colorbar[
-              Math.round(
-                Math.sqrt(nextposition.u ** 2 + nextposition.v ** 2) /
-                  (maxVelocity - minVelocity)
-              )
-            ];
-          colors[line.age * 4 - 4] = color[0];
-          colors[line.age * 4 - 3] = color[1];
-          colors[line.age * 4 - 2] = color[2];
-          for (let c = 1; c < line.age; c++) {
-            colors[c * 4 - 1] = Math.exp(1 - 1 / (c / line.age) ** 2);
-          }
-          line.data.geometry.attributes.color.needsUpdate = true;
-          line.data.geometry.setDrawRange(0, line.age);
-          line.data.geometry.attributes.position.needsUpdate = true;
-        } else {
-          line.age = line.maxAge - fadeOut;
-        }
-      } else if (line.age < line.maxAge) {
-        // Fade out line
-        line.age++;
-        for (let c = 1; c < line.age; c++) {
-          colors[c * 4 - 1] = Math.max(
-            colors[c * 4 - 1] - colors[c * 4 - 1] / (line.maxAge - line.age),
-            0
-          );
-        }
-        line.data.geometry.attributes.color.needsUpdate = true;
-      } else {
-        // Reset particle location
-        line.age = 0;
-        line.maxAge = Math.round((maxAge - fadeOut) * Math.random()) + fadeOut;
-        let pick = randomPick[Math.round(pl * Math.random())];
-        positions[0] = arr[pick[0]][0]; // x
-        positions[1] = depths[pick[1]]; // z
-        //positions[1] = 0; // z
-        positions[2] = -arr[pick[0]][1]; // -y
-        positions[3] = positions[0] + 0.1;
-        positions[4] = positions[1] + 0.1;
-        positions[5] = positions[2] + 0.1;
-        for (let c = 0; c < maxAge * 4; c++) {
-          colors[c] = 1;
-        }
-        line.data.geometry.setDrawRange(0, line.age);
-        line.data.geometry.attributes.position.needsUpdate = true;
-        line.data.geometry.attributes.color.needsUpdate = true;
-      }
-    }
-  };
-
   removeOuterTriangles(indexDelaunay, maxVertex) {
     let newTriangles = [];
     for (let k = 0; k < indexDelaunay.triangles.length; k += 3) {
@@ -406,8 +273,8 @@ class Home extends Component {
       1000 // far plane
     );
     this.camera.position.z = 55;
-    this.camera.position.x = 5;
-    this.camera.position.y = 15;
+    this.camera.position.x = 15;
+    this.camera.position.y = 35;
     this.controls = new OrbitControls(this.camera, this.mount);
     this.controls.maxPolarAngle = Math.PI / 2;
     this.renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -417,9 +284,8 @@ class Home extends Component {
     var light = new THREE.AmbientLight(0x404040);
     this.scene.add(light);
 
-    this.stats = new Stats();
-    //this.stats.showPanel( 1 );
-    document.body.appendChild(this.stats.domElement);
+    //this.stats = new Stats();
+    //document.body.appendChild(this.stats.domElement);
   };
 
   addMesh = () => {
@@ -484,6 +350,116 @@ class Home extends Component {
     }
   };
 
+  nextPosition = (xin, yin, zin) => {
+    let {
+      xSize,
+      ySize,
+      xllcorner,
+      yllcorner,
+      nCols,
+      nRows,
+      griddata,
+      velocityFactor,
+      depths,
+    } = this.state;
+    var zi = this.indexOfClosest(zin, depths);
+    var i = Math.round((yin - yllcorner) / ySize);
+    var j = Math.round((xin - xllcorner) / xSize);
+    if (i > -1 && i < nRows && j > -1 && j < nCols && griddata[i][j] !== null) {
+      var u = 0;
+      var v = 0;
+      if (griddata[i][j][0].length > 0 && griddata[i][j][0][zi] !== -999)
+        u = griddata[i][j][0][zi];
+      if (griddata[i][j][1].length > 0 && griddata[i][j][1][zi] !== -999)
+        v = griddata[i][j][1][zi];
+      var x = xin + u * velocityFactor;
+      var y = yin + -v * velocityFactor;
+      return { x, y, z: zin, u, v };
+    } else {
+      return false;
+    }
+  };
+
+  updatePositions = () => {
+    var {
+      arr,
+      randomPick,
+      depths,
+      minVelocity,
+      maxVelocity,
+      colorbar,
+      maxAge,
+      fadeOutPercentage,
+    } = this.state;
+    var fadeOut = Math.round(maxAge * fadeOutPercentage);
+    var pl = randomPick.length - 1;
+    for (var i = 0; i < this.lines.children.length; i++) {
+      let line = this.lines.children[i];
+      let positions = line.geometry.attributes.position.array;
+      let colors = line.geometry.attributes.color.array;
+      if (line.age < line.maxAge - fadeOut) {
+        // Move to next position
+        line.age++;
+        var nextposition = this.nextPosition(
+          positions[(line.age - 1) * 3],
+          positions[(line.age - 1) * 3 + 2],
+          positions[(line.age - 1) * 3 + 1]
+        );
+        if (nextposition) {
+          positions[line.age * 3] = nextposition.x;
+          positions[line.age * 3 + 1] = nextposition.z;
+          positions[line.age * 3 + 2] = nextposition.y;
+          let color =
+            colorbar[
+              Math.round(
+                Math.sqrt(nextposition.u ** 2 + nextposition.v ** 2) /
+                  (maxVelocity - minVelocity)
+              )
+            ];
+          colors[line.age * 4 - 4] = color[0];
+          colors[line.age * 4 - 3] = color[1];
+          colors[line.age * 4 - 2] = color[2];
+          for (let c = 1; c < line.age; c++) {
+            colors[c * 4 - 1] = Math.exp(1 - 1 / (c / line.age) ** 2);
+          }
+          line.geometry.attributes.color.needsUpdate = true;
+          line.geometry.setDrawRange(0, line.age);
+          line.geometry.attributes.position.needsUpdate = true;
+        } else {
+          line.age = line.maxAge - fadeOut;
+        }
+      } else if (line.age < line.maxAge) {
+        // Fade out line
+        line.age++;
+        for (let c = 1; c < line.age; c++) {
+          colors[c * 4 - 1] = Math.max(
+            colors[c * 4 - 1] - colors[c * 4 - 1] / (line.maxAge - line.age),
+            0
+          );
+        }
+        line.geometry.attributes.color.needsUpdate = true;
+      } else {
+        // Reset particle location
+        line.age = 0;
+        line.maxAge = Math.round((maxAge - fadeOut) * Math.random()) + fadeOut;
+        let pick = randomPick[Math.round(pl * Math.random())];
+        positions[0] = arr[pick[0]][0]; // x
+        positions[1] = depths[pick[1]]; // z
+        //positions[1] = 0; // z
+        positions[2] = -arr[pick[0]][1]; // -y
+        positions[3] = positions[0] + 0.1;
+        positions[4] = positions[1] + 0.1;
+        positions[5] = positions[2] + 0.1;
+        for (let c = 0; c < maxAge * 4; c++) {
+          colors[c] = 1;
+        }
+        line.geometry.setDrawRange(0, line.age);
+        line.geometry.attributes.position.needsUpdate = true;
+        line.geometry.attributes.color.needsUpdate = true;
+      }
+    }
+  };
+
   addStreamlines = () => {
     var { noParticles, maxAge, fadeOutPercentage } = this.state;
 
@@ -525,7 +501,7 @@ class Home extends Component {
       new THREE.Float32BufferAttribute(colors, 4, true)
     );
 
-    var positions = new Float32Array(maxAge * 3); // 3 vertices per point
+    var positions = new Float32Array(maxAge * 3);
     geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
     geometry.setDrawRange(0, 0);
 
@@ -537,26 +513,23 @@ class Home extends Component {
       depthTest: false,
     });
 
-    this.lines = [];
-
-
+    this.lines = new THREE.Group();
     for (var p = 0; p < noParticles; p++) {
       let line = new THREE.Line(geometry.clone(), material.clone());
-      this.lines.push({
-        data: line,
-        age: 0,
-        maxAge: Math.round((maxAge - fadeOut) * Math.random()) + fadeOut,
-      });
-      this.scene.add(line);
+      line.age = 0;
+      line.maxAge = Math.round((maxAge - fadeOut) * Math.random()) + fadeOut;
+      this.lines.add(line);
     }
+    this.scene.add(this.lines);
 
-    // update positions
     this.initialPositions();
   };
 
   removeStreamlines = () => {
-    for (var line of this.lines) {
-      this.scene.remove(line.data);
+    try {
+      this.scene.remove(this.lines);
+    } catch (e) {
+      console.error("Failed to remove streamlines", e);
     }
   };
 
@@ -564,12 +537,14 @@ class Home extends Component {
     this.updatePositions();
     this.renderer.render(this.scene, this.camera);
     this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
-    this.stats.update();
+    //this.stats.update();
   };
 
-  prepareData = async (quadtreeSensitivity, lake) => {
-    var { depths, arr } = await this.downloadLake(lake);
-    ({ depths, arr } = this.globalToLocalCoordinate(depths, arr));
+  processData = async (quadtreeSensitivity, lakes, lake) => {
+    var { depths, arr } = this.globalToLocalCoordinate(
+      lakes[lake].data.depths,
+      lakes[lake].data.arr
+    );
     var colorbar = this.generateColorBar(arr);
     var {
       randomPick,
@@ -593,46 +568,6 @@ class Home extends Component {
     subtext.innerHTML = "Plotting velocity field... ";
     subtext.classList.add("fade-out");
     document.getElementById("text").classList.add("fade-out");
-    return {
-      nCols,
-      nRows,
-      xSize,
-      ySize,
-      xllcorner,
-      yllcorner,
-      griddata,
-      arr,
-      depths,
-      randomPick,
-      bottomSurface,
-      quadtree,
-      colorbar,
-      minVelocity,
-      maxVelocity,
-    };
-  };
-
-  async componentDidMount() {
-    var { quadtreeSensitivity, lake } = this.state;
-
-    var {
-      nCols,
-      nRows,
-      xSize,
-      ySize,
-      xllcorner,
-      yllcorner,
-      griddata,
-      arr,
-      depths,
-      randomPick,
-      bottomSurface,
-      quadtree,
-      colorbar,
-      minVelocity,
-      maxVelocity,
-    } = await this.prepareData(quadtreeSensitivity, lake);
-
     this.setState(
       {
         nCols,
@@ -650,18 +585,26 @@ class Home extends Component {
         colorbar,
         minVelocity,
         maxVelocity,
+        lakes,
+        update: false,
       },
       () => {
         setTimeout(() => {
           this.setState({ loaded: true });
         }, 2000);
-        this.sceneSetup();
         this.addMesh();
         this.addStreamlines();
         this.startAnimationLoop();
-        window.addEventListener("resize", this.handleWindowResize);
       }
     );
+  };
+
+  async componentDidMount() {
+    var { quadtreeSensitivity, lake, lakes } = this.state;
+    lakes[lake].data = await this.downloadLake(lake);
+    this.sceneSetup();
+    window.addEventListener("resize", this.handleWindowResize);
+    this.processData(quadtreeSensitivity, lakes, lake);
   }
 
   componentWillUnmount() {
@@ -670,55 +613,30 @@ class Home extends Component {
     this.controls.dispose();
   }
 
-  async componentDidUpdate(prevState) {
+  async componentDidUpdate() {
     var { update } = this.state;
     if (update) {
       this.removeStreamlines();
+      this.removeMesh();
+      window.cancelAnimationFrame(this.requestID);
+      this.addMesh();
+      this.addStreamlines();
+      this.startAnimationLoop();
       this.setState({ update: false });
     }
   }
 
   render() {
-    var {
-      loaded,
-      xScale,
-      yScale,
-      zScale,
-      noParticles,
-      velocityFactor,
-      maxAge,
-      mesh,
-    } = this.state;
+    var { loaded, noParticles, velocityFactor, maxAge, mesh } = this.state;
     document.title = "Dynamic Lakes";
     return (
       <React.Fragment>
         <div className="main">
           {loaded && (
             <div className="about fade-in">
-              Dynamic lakes uses the output simulations results from the
+              Dynamic lakes uses the output simulation results from the
               Meteolakes project, in order to display 3D stream lines.
               <div className="plotparameters">
-                <div className="plotrow">
-                  Scale
-                  <ScaleBox
-                    s="xScale"
-                    label="x"
-                    value={xScale}
-                    onChange={this.onChangeState}
-                  />
-                  <ScaleBox
-                    s="yScale"
-                    label="y"
-                    value={yScale}
-                    onChange={this.onChangeState}
-                  />
-                  <ScaleBox
-                    s="zScale"
-                    label="z"
-                    value={zScale}
-                    onChange={this.onChangeState}
-                  />
-                </div>
                 <div className="plotrow">
                   View Boundary
                   <input
@@ -734,7 +652,14 @@ class Home extends Component {
                     type="number"
                     value={noParticles}
                     onChange={(e) => this.onChangeState("noParticles", e)}
+                    title="WARNING! Unless your PC is super powerful more than 10000 streams is likely to crash your browser."
                   />
+                  <button
+                    onClick={this.update}
+                    title="Update number of streams"
+                  >
+                    &#8635;
+                  </button>
                 </div>
                 <div className="plotrow">
                   Velocity{" "}
@@ -753,9 +678,6 @@ class Home extends Component {
                     value={maxAge}
                     onChange={(e) => this.onChangeState("maxAge", e)}
                   />
-                </div>
-                <div className="plotrow">
-                  <button onClick={this.update}>Update Plot</button>
                 </div>
               </div>
             </div>
